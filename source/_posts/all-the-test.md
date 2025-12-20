ce348d5a44e041217a0d48183a1714e1b7042ee5
JoyWonderful <99643089+JoyWonderful@users.noreply.github.com>
2025-12-20 22:53:07 +0800

    Change to cloudflare.

--- source/_posts/all-the-test.md
+++ source/_posts/all-the-test.md
@@ -45,7 +45,7 @@ mathjax: true
 ```
 
 ##### 五级标题
-![favicon](https://src-jywon.netlify.app/favicon.svg)
+![favicon](https://src-jywon.pages.dev/favicon.svg)
 [Back to home](/)  
 [404 check](/404A114514)
 
--- themes/lite/scripts/helpers.js
+++ themes/lite/scripts/helpers.js
@@ -5,4 +5,10 @@
 const moment = require("moment");
 hexo.extend.helper.register('post_time', function(momObject, formatStr) {
     return moment(momObject).format(formatStr);
-})
+});
+hexo.extend.helper.register('src', function(file) {
+    if(file[0] == "/" && hexo.config.src_site.endsWith("/")) {
+        file = file.replace("/", "");
+    }
+    return hexo.config.src_site + file;
+});
