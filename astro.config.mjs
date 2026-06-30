f6cd1458001d01e7d69b3fa384db42acb0fe3444
JoyWonderful <99643089+JoyWonderful@users.noreply.github.com>
2026-06-30 21:52:39 +0800

    build(astro): Update redirection.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,11 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    redirects: {
+        "/": {
+            status: 301, // Moved Permanently
+            destination: "/1"
+        }
+    }
+});
