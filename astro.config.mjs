25b255899a088ba8ad30b59ec6c0a7f8556a3085
JoyWonderful <99643089+JoyWonderful@users.noreply.github.com>
2026-07-01 15:51:33 +0800

    feat: Parse external links specially. Add rel="noopener noreferer" & icon.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import { unified } from "@astrojs/markdown-remark";
+import rehypeExternalLinks from "rehype-external-links"; // rehype 管 HTML，remark 管 Markdown.
 
 // https://astro.build/config
 export default defineConfig({
@@ -8,5 +10,34 @@ export default defineConfig({
             status: 301, // Moved Permanently
             destination: "/1"
         }
+    },
+    markdown: {
+        processor: unified({
+            rehypePlugins: [
+                [rehypeExternalLinks, {
+                    rel: ["noopener noreferer"],
+                    target: "_blank",
+                    contentProperties: {
+                        class: "external-link-icon"
+                    },
+                    content: [{
+                        type: "element",
+                        tagName: "i",
+                        properties: {
+                            class: "fa fa-up-right-from-square"
+                        }
+                    }]
+                    // https://github.com/syntax-tree/hast#readme-ov-file
+                    /* content:
+                    interface Element <: Parent {
+                        type: 'element'
+                        tagName: string
+                        properties: Properties
+                        content: Root?
+                        children: [Comment | Element | Text]
+                    }*/
+                }]
+            ]
+        })
     }
 });
