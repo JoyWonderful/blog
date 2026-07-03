82141936a868dddfec01c793cf27d581720e95c0
JoyWonderful <99643089+JoyWonderful@users.noreply.github.com>
2026-07-03 23:28:32 +0800

    chore: Change source-path config schema.

--- src/scripts/helpers/source-path.ts
+++ src/scripts/helpers/source-path.ts
@@ -2,15 +2,15 @@ import config from "~/config.source.json";
 
 /**
  * @description 根据 config.source.json 获取需要载入的资源列表
- * @param {Array<number>} index 需要获取的 index（用于 `config[index.item]`）
+ * @param {Array<string>} identifiers 需要获取的 identifiers（用于 `config[index.item]`）
  * @returns {Array<string>}
  */
-export function getSourcePathList(index:Array<number>) {
+export function getSourcePathList(identifiers:Array<string>) {
     var lst:Array<string> = [];
-    for(const i in index) {
+    for(const i of identifiers) {
         var reg = config[i];
         var base_url = (new URL(reg.base_url)).origin; // 确保没有 `/` 结尾
-        reg.package_list.forEach((pkg) => {
+        reg.package_list.forEach((pkg:{package:string, version:string, file_path:Array<string>}) => {
             pkg.file_path.forEach((file) => {
                 var path_res = reg.path_format.replace(":package", pkg.package)
                                               .replace(":version", pkg.version)
